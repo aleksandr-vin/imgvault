c861ac5db7914901aa08d732ccae7426bc8fc425
bob <bob@ubegemot.(none)>
2013-04-15 01:31:58 +0400

    Add validation to ''image'' model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,8 @@
 class Image < ActiveRecord::Base
   attr_accessible :author, :title
+
+  validates :author, :presence => true
+  validates :title,  :presence => true,
+                     :length => { :minimum => 5,
+                                  :maximum => 140 }
 end
