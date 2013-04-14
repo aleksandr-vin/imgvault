bc47679e430b5b5f6f92266e5d0717a91518e8c9
bob <bob@ubegemot.(none)>
2013-04-14 18:04:52 +0400

    Init commit: new rails (3.2.13) app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imgvault::Application.config.secret_token = 'faa0c7290e0d56f6ebcd58dff77e3b02adae60769031777c7f671a0450078b07c2fc0a228d3b0f13d44e401e294c28980afa511c07905edf4a9a1f0c87e3d80c'
