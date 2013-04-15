0bbaa6c1a7ade1c84903bee43be66daf4b022ce7
Aleksandr Vinokurov <aleksandr.vinokurov@billing.ru>
2013-04-15 16:10:17 +0400

    Add unit tests for ''image'' and ''comment'' models validations

--- test/unit/comment_test.rb
+++ test/unit/comment_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save comment without valid body" do
+    comment = Comment.new :commenter => "John"
+    assert !comment.save, "Saved the comment without body"
+    comment.body = "!"
+    assert !comment.save, "Saved the comment with small body"
+    comment.body = "A" * 141
+    assert !comment.save, "Saved the comment with big body"
+    comment.body = "Valid body greater than 2 chars"
+    assert comment.save, "Not saved the full crafted comment"
+  end
+
+  test "should not save comment without commenter" do
+    comment = Comment.new :body => "Valid body greater than 2 chars"
+    assert !comment.save, "Saved the comment without commenter"
+    comment.commenter = "Jay"
+    assert comment.save, "Not saved the full crafted comment"
+  end
 end
--- test/unit/image_test.rb
+++ test/unit/image_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class ImageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save image without valid title" do
+    img = Image.new :author => "John"
+    assert !img.save, "Saved the image without title"
+    img.title = "123"
+    assert !img.save, "Saved the image with small title"
+    img.title = "A" * 141
+    assert !img.save, "Saved the image with big title"
+    img.title = "Valid title greater than 5 chars"
+    assert img.save, "Not saved the full crafted image"
+  end
+
+  test "should not save image without author" do
+    img = Image.new :title => "Valid title greater than 5 chars"
+    assert !img.save, "Saved the image without author"
+    img.author = "Jay"
+    assert img.save, "Not saved the full crafted image"
+  end
 end
